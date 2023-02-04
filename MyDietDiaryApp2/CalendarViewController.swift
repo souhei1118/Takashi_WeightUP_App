2ba5151e01fa38a6e260c7753e4632b03339b3e3
髙師蒼平 <so.takashi1118@gmail.com>
2023-02-04 15:32:38 +0900

    FSCalendarの表示

---
+++ MyDietDiaryApp2/CalendarViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarViewController.swift
+//  MyDietDiaryApp2
+//
+//  Created by 高師蒼平 on 2023/02/04.
+//
+
+import UIKit
+import FSCalendar
+
+class CalendarViewController: UIViewController {
+    @IBOutlet weak var calendarView: FSCalendar!
+    
+}
---
+++ MyDietDiaryApp2/GraphViewController.swift
@@ -0,0 +1,12 @@
+//
+//  GraphViewController.swift
+//  MyDietDiaryApp2
+//
+//  Created by 高師蒼平 on 2023/02/04.
+//
+
+import UIKit
+
+class GraphViewController: UIViewController {
+    
+}
