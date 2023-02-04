3a13c2e876a92db8cd1d8b185b2d9600abe3cb7e
髙師蒼平 <so.takashi1118@gmail.com>
2023-02-04 15:53:21 +0900

    [WIP]Calendarのテキスト、カラーを変更

--- MyDietDiaryApp2/CalendarViewController.swift
+++ MyDietDiaryApp2/CalendarViewController.swift
@@ -11,4 +11,28 @@ import FSCalendar
 class CalendarViewController: UIViewController {
     @IBOutlet weak var calendarView: FSCalendar!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureCalendar()
+    }
+    
+    func configureCalendar() {
+        calendarView.appearance.headerDateFormat =
+        "yyyy年MM月"
+        
+        calendarView.appearance.todayColor = .orange
+        calendarView.appearance.headerTitleColor = .orange
+        calendarView.appearance.weekdayTextColor = .black
+        
+        calendarView.calendarWeekdayView.weekdayLabels[0].text = "日"
+        calendarView.calendarWeekdayView.weekdayLabels[1].text = "月"
+        calendarView.calendarWeekdayView.weekdayLabels[2].text = "火"
+        calendarView.calendarWeekdayView.weekdayLabels[3].text = "水"
+        calendarView.calendarWeekdayView.weekdayLabels[4].text = "木"
+        calendarView.calendarWeekdayView.weekdayLabels[5].text = "金"
+        calendarView.calendarWeekdayView.weekdayLabels[6].text = "土"
+        
+        calendarView.calendarWeekdayView.weekdayLabels[0].textColor = .red
+        calendarView.calendarWeekdayView.weekdayLabels[6].textColor = .blue
+    }
 }
