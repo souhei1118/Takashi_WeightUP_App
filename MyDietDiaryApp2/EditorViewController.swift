f31bce235a9bf5d244f4efe670ed9679ebfaa4a2
髙師蒼平 <so.takashi1118@gmail.com>
2023-02-12 20:06:04 +0900

    [add]日付と体重のテキストフィールドを設定し、それぞれのフォーマットを指定

--- MyDietDiaryApp2/EditorViewController.swift
+++ MyDietDiaryApp2/EditorViewController.swift
@@ -8,5 +8,48 @@
 import UIKit
 
 class EditorViewController: UIViewController {
+    @IBOutlet weak var inputWeightTextField: UITextField!
+    @IBOutlet weak var inputDateTextField: UITextField!
     
+    
+    var datePicker: UIDatePicker {
+        let datePicker: UIDatePicker = UIDatePicker()
+        datePicker.datePickerMode = .date
+        datePicker.timeZone = .current
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.locale = Locale(identifier: "ja-JP")
+        datePicker.date = Date()
+        return datePicker
+    }
+    
+    var toolbar: UIToolbar {
+        let toolBarRect = CGRect(x: 0, y: 0, width: view.frame.size.width, height: 35)
+        let toolBar =  UIToolbar(frame: toolBarRect)
+        let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(didTapDone))
+        toolBar.setItems([doneItem], animated: true)
+        return toolBar
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureWeightTextField()
+        configureDateTextField()
+    }
+    
+    @objc func didTapDone() {
+        view.endEditing(true)
+    }
+    
+    func configureWeightTextField() {
+        let toolBarRect = CGRect(x: 0, y: 0, width: view.frame.size.width, height: 35)
+        let toolBar =  UIToolbar(frame: toolBarRect)
+        let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(didTapDone))
+        toolBar.setItems([doneItem], animated: true)
+        inputWeightTextField.inputAccessoryView = toolBar
+    }
+    
+    func configureDateTextField() {
+        inputDateTextField.inputView = datePicker
+        inputDateTextField.inputAccessoryView = toolbar
+    }
 }
--- MyDietDiaryApp2/WeightRecord.swift
+++ MyDietDiaryApp2/WeightRecord.swift
@@ -7,3 +7,12 @@
 
 import Foundation
 import RealmSwift
+
+class WeightRecord: Object {
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+    @objc dynamic var id: String = UUID().uuidString
+    @objc dynamic var date: Date = Date()
+    @objc dynamic var weight: Double = 0
+}
